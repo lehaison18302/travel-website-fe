35e01d88e76840565b4472edf4e7bb27948bd211
CaoDuc <Caominhducpx@gmail.com>
2025-01-06 18:00:01 +0700

    1 con vịt

--- src/components/Account/IndexAccount.jsx
+++ src/components/Account/IndexAccount.jsx
@@ -1,43 +1,21 @@
 import { Avatar, Card, Tabs } from "antd";
 import { useState } from "react";
 import { listAVT } from "src/constants/ListAvatar";
+import Information from "./Information";
+import HistoryVote from "./HistoryVote";
 
 const listMenu = [
   {
     id: 0,
     key: 0,
     label: <strong>Thông tin cá nhân</strong>,
-    children: (
-      <div className="account-info-card">
-        <Card>
-          <Card.Meta
-            title={<strong style={{ fontSize: 18 }}>Hải sơn</strong>}
-            avatar={
-              <Avatar
-                size={{
-                  xs: 24,
-                  sm: 32,
-                  md: 40,
-                  lg: 64,
-                  xl: 80,
-                  xxl: 100
-                }}
-                src={listAVT[0]?.src}
-              />
-            }
-            description={
-              <span>spa"email: kasdhdasasdhjkhjkasdhjkasdasdhjkhjk"</span>
-            }
-          />
-        </Card>
-      </div>
-    )
+    children: <Information />
   },
   {
     id: 1,
     key: 1,
     label: <strong>Lịch sử đánh giá</strong>,
-    children: <>abc</>
+    children: <HistoryVote />
   },
   {
     id: 2,
@@ -52,7 +30,6 @@ const IndexAccount = () => {
     <div className="account-infor-container">
       <div className="account-info-menu">
         <Tabs
-          title="abc"
           size="large"
           tabPosition="left"
           items={listMenu}
